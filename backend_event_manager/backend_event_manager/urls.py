6869a1fd48bc55e24af6dc44648e6fc25fb792a4
unknown <peterjheile@gmail.com>
2024-12-31 11:20:44 -0500

    same

--- backend_event_manager/backend_event_manager/urls.py
+++ backend_event_manager/backend_event_manager/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend_event_manage project.
+URL configuration for backend_event_manager project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
